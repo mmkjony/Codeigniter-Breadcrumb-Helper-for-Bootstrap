e6c276cd3db88d9b3433b6ba6543082404d3af50
MMK Jony <mmkjony@users.noreply.github.com>
2016-10-04 11:32:11 +0600

    Re-built
    Re-built the module. Initial icon can be added now.

--- breadcrumb_helper.php
+++ breadcrumb_helper.php
@@ -13,14 +13,14 @@
  *
  **/
 
-if (!function_exists('ci_breadcrumb'))
+if ( !function_exists( 'ci_breadcrumb' ) )
 {
-    function ci_breadcrumb($initial_crumb = '', $initial_crumb_url = '')
+    function ci_breadcrumb( $initial_crumb = FALSE, $initial_crumb_url = FALSE, $initial_crumb_icon = FALSE )
     {
         $ci = &get_instance();
 
-        $open_tag = '<ol class="breadcrumb">';
-        $close_tag = '</ol>';
+        $open_tag = '<ul class="breadcrumb">';
+        $close_tag = '</ul>';
         $crumb_open_tag = '<li>';
         $active_crumb_open_tag = '<li class="active">';
         $crumb_close_tag = '</li>';
@@ -29,27 +29,27 @@ if (!function_exists('ci_breadcrumb'))
 
         $breadcrumbs = $open_tag;
 
-        if ($initial_crumb != '') {
+        if ( $initial_crumb ) {
             $breadcrumbs .= $crumb_open_tag;
-            $breadcrumbs .= ci_breadcrumb_href($initial_crumb, false, true);
+            $breadcrumbs .= ci_breadcrumb_href( $initial_crumb, $initial_crumb_url, TRUE, TRUE, $initial_crumb_icon );
         }
 
         $segment = '';
         $crumb_href = '';
 
-        for ($i = 1; $i <= $total_segments; $i++)
+        for ( $i = 1; $i <= $total_segments; $i++ )
         {
-            $segment = $ci->uri->segment($i);
-            $crumb_href .= $ci->uri->segment($i) . '/';
+            $segment = $ci->uri->segment( $i );
+            $crumb_href .= $ci->uri->segment( $i ) . '/';
 
-            if ($total_segments > $i)
+            if( $total_segments > $i )
             {
                 $breadcrumbs .= $crumb_open_tag;
-                $breadcrumbs .= ci_breadcrumb_href($segment, $crumb_href);
-            }else
+                $breadcrumbs .= ci_breadcrumb_href( $segment, $crumb_href );
+            } else
             {
                 $breadcrumbs .= $active_crumb_open_tag;
-                $breadcrumbs .= ci_breadcrumb_href($segment, $crumb_href, false, false);
+                $breadcrumbs .= ci_breadcrumb_href( $segment, $crumb_href, FALSE, FALSE );
             }
             $breadcrumbs .= $crumb_close_tag;
         }
@@ -59,26 +59,25 @@ if (!function_exists('ci_breadcrumb'))
     }
 }
 
-if (!function_exists('ci_breadcrumb_href'))
+if ( !function_exists( 'ci_breadcrumb_href' ) )
 {
-    function ci_breadcrumb_href($uri_segment, $crumb_href = false, $initial = false, $active_link = true)     {
+    function ci_breadcrumb_href( $uri_segment, $crumb_href = FALSE, $initial = FALSE, $active_link = TRUE, $crumb_icon = FALSE )     {
         $ci = &get_instance();
-        $base_url = $ci->config->base_url();
 
-        $crumb_href = rtrim($crumb_href, '/');
+        $crumb_href = rtrim( $crumb_href, '/' );
 
-        if($active_link)
+        if( $active_link )
         {
-            if($initial)
+            if( $initial )
             {
-                return '<a href="' . $base_url . '">' . strtolower(str_replace(array('-', '_'), ' ', $uri_segment)) . '</a>';
-            }else
+                return ( $crumb_icon ? '<span class="'.$crumb_icon.'"></span> ' : '' ) . '<a href="' . ( $crumb_href ? $crumb_href : site_url() ) . '">' . ucwords( str_replace( array( '-', '_' ), ' ', $uri_segment ) ) . '</a>';
+            } else
             {
-                return '<a href="' . $base_url . $crumb_href . '">' . strtolower(str_replace(array('-', '_'), ' ', $uri_segment)) . '</a>';
+                return ( $crumb_icon ? '<span class="'.$crumb_icon.'"></span> ' : '' ) . '<a href="' . site_url( $crumb_href ) . '">' . ucwords( str_replace( array( '-', '_' ), ' ', $uri_segment ) ) . '</a>';
             }
-        }else
+        } else
         {
-            return strtolower(str_replace(array('-', '_'), ' ', $uri_segment));
+            return ( $crumb_icon ? '<span class="'.$crumb_icon.'"></span> ' : '' ) . ucwords( str_replace( array( '-', '_' ), ' ', $uri_segment ) );
         }
     }
 }
